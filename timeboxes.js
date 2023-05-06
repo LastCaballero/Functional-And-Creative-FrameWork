8c0cfb80ce423b62de2b9270524001c526964ee4
Goran Mladenovic <59745165+LastCaballero@users.noreply.github.com>
2023-05-06 15:03:00 +0200

    Update timeboxes.js

--- timeboxes.js
+++ timeboxes.js
@@ -76,7 +76,7 @@ class TimeButton {
         this.Button = document.createElement('Button')
         this.Button.innerHTML = inner
         this.Button.onclick = () => {
-            if( this.Stamps.push(new Date().getMilliseconds()) > 1 ) {
+            if( this.Stamps.push(new Date().getTime()) > 1 ) {
                 this.SetP1()
             } 
             
@@ -84,6 +84,7 @@ class TimeButton {
     }
     constructDiv() {
         this.DIV = document.createElement('div')
+        this.DIV.style.flexDirection = "row"
         this.DIV.append(this.Button, this.P1, this.P2)
     }
     constructor(btn_inner) {
@@ -93,12 +94,14 @@ class TimeButton {
         this.constructButton(btn_inner)
         this.constructDiv()
     }
-    GetDiv() { return this.DIV }
+    Get(){ return this.DIV }
     GetStamps() { return this.Stamps }
     GetCalculated() {
-        let reversed = this.Stamps.reverse()
-        let accumulate = (acm, curv) => acm + curv
-        return ((reversed.reduce(accumulate, 0) / reversed.length) / 1000)
+        let sum = 0
+        for(let i = 0 ; i < this.Stamps.length -1 ; i++){
+            sum += this.Stamps[i+1] - this.Stamps[i] 
+        }
+        return ((sum / this.Stamps.length) / 1000).toFixed(3)
     }
     SetP1() {
         this.P1.innerHTML = this.GetCalculated()
