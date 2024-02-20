86ce4d7d621bb49bc1ec6ddde13f13efe2d8f49d
Goran Mladenovic <59745165+LastCaballero@users.noreply.github.com>
2024-02-20 15:14:34 +0100

    Update ease.js

--- ease.js
+++ ease.js
@@ -1,15 +1,19 @@
-let ce = ( what ) => document.createElement( what )
-
+function Standard( what, inner, enrichment ){
+    let comp = document.createElement( what )
+    comp.innerText = inner
+    Object.assign( comp, enrichment )
+    return comp ;
+}
 
 function ActionButton( inner, action ) {
-    let button = ce('button')
+    let button = document.createElement('button')
     button.innerText = inner
     button.onclick = action
     return button
 }
 
 function CounterButton() {
-    let button = ce('button')
+    let button = document.createElement('button')
     let val = 0
     button.innerText = val
     button.onclick = () => {
@@ -19,8 +23,18 @@ function CounterButton() {
     return button
 }
 
+function CounterButtonGroup( n ) {
+    let buttons = new Array( n )
+    for (let index = 0; index < buttons.length; index++) {
+        buttons[index] = CounterButton()
+    }
+    let box = document.createElement('div')
+    buttons.forEach( button => box.append( button ) )
+    return box
+}
+
 function Seconds() {
-    let par = ce('p')
+    let par = document.createElement('p')
     let digit
     window.setInterval(() => {
         digit = new Date().getSeconds()
@@ -31,7 +45,7 @@ function Seconds() {
 }
 
 function Minutes() {
-    let par = ce('p')
+    let par = document.createElement('p')
     let digit
     window.setInterval(() => {
         digit = new Date().getMinutes()
@@ -42,7 +56,7 @@ function Minutes() {
 }
 
 function Hours() {
-    let par = ce('p')
+    let par = document.createElement('p')
     let digit
     window.setInterval(() => {
         digit = new Date().getHours()
